08ff464e90c73f3f3d96b9b90b4d90b32d97975d
Brayden Streibel <brayden.streibel@fcc.ca>
2020-05-11 15:34:47 -0600

    :construction: Appears to work with Tomcat.

--- src/main/java/com/example/demo/FooController.java
+++ src/main/java/com/example/demo/FooController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
+import java.time.Duration;
 import java.util.function.Consumer;
 
 @Slf4j
@@ -24,24 +25,38 @@ public class FooController {
 	@Autowired
 	private PublishSubscribeChannel countChannel;
 
-	@GetMapping(value = "/listen", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public Flux<?> listen() {
+	@GetMapping(value = "/listen-event", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<?> listenOnEvent() {
 		log.info("Creating new flux");
 		return Flux.create(getFluxSinkConsumer())
 				.log()
-				.doOnError(e -> log.info("doOnError"))
-				.doOnTerminate(() -> log.info("doOnTerminate"))
-				.doOnCancel(() -> log.info("doOnCancel"))
-				.doOnComplete(() -> log.info("doOnComplete"))
-				.doOnDiscard(Foo.class, x -> log.info("doOnDiscard"))
-				.doFinally(s -> log.info("doFinally"))
-				.map(count -> ServerSentEvent.builder(count).build());
+				.doFinally(s -> doFinally())
+				.map(this::createServerSentEvent);
+	}
+
+	@GetMapping(value = "/listen-interval", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<?> listenOnInterval() {
+		log.info("Creating new flux");
+		return Flux.interval(Duration.ofSeconds(1))
+				.log()
+				.doFinally(s -> doFinally())
+				.map(x -> Foo.newInstance())
+				.map(this::createServerSentEvent);
 	}
 
 	@PostMapping(value = "/publish")
 	public void publish() {
 		log.info("Publishing new event");
 		applicationEventPublisher.publishEvent(new FooEvent(Foo.newInstance()));
+	}
+
+	void doFinally() {
+		log.info("doFinally");
+	}
+
+	ServerSentEvent<Foo> createServerSentEvent(Foo foo) {
+		log.info("createServerSentEvent [{}]", foo);
+		return ServerSentEvent.builder(foo).build();
 	}
 
 	private Consumer<FluxSink<Foo>> getFluxSinkConsumer() {
@@ -51,14 +66,13 @@ public class FooController {
 				sink.next(Foo.newInstance());
 			};
 			sink.onDispose(() -> {
-				log.info("Disposed!");
+				log.info("FluxSink.onDispose()");
 				countChannel.unsubscribe(handler);
 			});
 			sink.onCancel(() -> {
-				log.info("Cancelled!");
+				log.info("FluxSink.onCancel()");
 				countChannel.unsubscribe(handler);
 			});
-			log.info("Subscribed!");
 			countChannel.subscribe(handler);
 		};
 	}
